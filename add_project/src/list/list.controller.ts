4115d7dfea35c04177c0b6ced2efe2f434bfafc0
tanchian <tanchian@shencom.cn>
2024-03-15 15:32:07 +0800

    feat: add guard case

--- add_project/src/list/list.controller.ts
+++ add_project/src/list/list.controller.ts
@@ -6,26 +6,45 @@ import {
   Patch,
   Param,
   Delete,
+  Req,
+  UseGuards,
+  SetMetadata,
 } from '@nestjs/common';
 import { ListService } from './list.service';
 import { CreateListDto } from './dto/create-list.dto';
 import { UpdateListDto } from './dto/update-list.dto';
+import { ListPipe } from './list.pipe';
+import { Request } from 'express';
+import { RoleGuard } from './list.guard';
 
 @Controller({
   path: 'list',
   version: '1',
 })
+@UseGuards(RoleGuard)
 export class ListController {
   constructor(private readonly listService: ListService) {}
 
   @Post()
-  create(@Body() createListDto: CreateListDto) {
+  create(@Body(ListPipe) createListDto: CreateListDto) {
+    console.log(
+      '%c [createListDto]-24',
+      'font-size:13px; background:#336699; color:#fff;',
+      createListDto,
+    );
     return this.listService.create(createListDto);
   }
 
   @Get()
   findAll() {
     return 123;
+  }
+
+  @Get('test')
+  @SetMetadata('role', ['admin'])
+  getIp(@Req() request: Request): string {
+    const ipAddress = request.ip;
+    return `Your IP address is ${ipAddress}`;
   }
 
   @Get(':id')
